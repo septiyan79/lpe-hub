0acf9526f996f5f2714e865a004dc296a5f34265
Septyan Eka Prastyio <4885@dnpi.co.id>
2026-01-27 16:44:34 +0700

    benahi Todolist (TAMPILAN)

--- src/general/pages/ToDoList_Tailwind.jsx
+++ src/general/pages/ToDoList_Tailwind.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Plus, Zap } from 'lucide-react';
+import { Plus, Zap, Save, Pencil, Trash2 } from 'lucide-react';
 
 export default function ToDoList() {
     // state: todos = { id, text, done }
@@ -14,7 +14,7 @@ export default function ToDoList() {
 
     const [input, setInput] = useState("");
     const [editingId, setEditingId] = useState(null);
-    const [filter, setFilter] = useState("all");
+    const [filter, setFilter] = useState("active");
 
 
     //simpan ke Local Storage dengan pemicu perubahan pada state todos ([todos])
@@ -107,9 +107,9 @@ export default function ToDoList() {
                 </div>
 
 
-                <div className="md:flex-row items-center justify-between mb-12
-                    bg-white/40 backdrop-blur-md p-4 rounded-3xl 
-                    border border-white/60 shadow-sm
+                <div className="md:flex-row items-center justify-between mb-3
+                    bg-white backdrop-blur-md p-4 rounded-3xl 
+                    border border-white shadow-sm
                     w-full md:w-auto"
                 >
                     <div className="m-3">
@@ -130,7 +130,11 @@ export default function ToDoList() {
                                         cancelEdit();
                                     }
                                 }}
-                                className="flex-1 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 text-amber-900 focus:ring-amber-200 border-amber-700"
+                                className={`flex-1 border-b border-r border rounded-md px-3 py-2 focus:outline-none focus:ring-2 
+                                        ${!editingId
+                                        ? "text-orange-900 focus:ring-orange-200 border-orange-100"
+                                        : "text-cyan-900 focus:ring-cyan-200 border-cyan-100"
+                                    }`}
                                 placeholder="Add new Todo"
                             />
 
@@ -139,99 +143,113 @@ export default function ToDoList() {
                                     <>
                                         <button
                                             onClick={saveEdit}
-                                            className="px-3 py-2 rounded-md bg-amber-600 text-amber-100 hover:bg-amber-500"
-                                        > Save </button>
+                                            className="px-3 py-2 flex items-center gap-2 font-medium rounded-md bg-cyan-100 text-cyan-800 hover:bg-cyan-200"
+                                        > <Save size={15} /> Save </button>
                                     </>
                                 ) : (
                                     <button
                                         onClick={addTodo}
-                                        className="px-3 py-2 rounded-md bg-amber-600 text-amber-100 hover:bg-amber-500"
-                                    > Add </button>
+                                        className="px-3 py-2 flex items-center gap-2 font-medium rounded-md bg-orange-500 text-orange-200 hover:text-orange-700 hover:bg-orange-300"
+                                    > <Plus size={15} /> Add List </button>
                                 )}
                             </div>
                         </div>
 
-                        {/* controls */}
-                        <div className="flex flex-col sm:flex-row sm:justify-between items-start sm:items-center gap-3 mb-4 text-white">
-                            <div className="flex gap-2">
-                                <button
-                                    onClick={() => setFilter("all")}
-                                    className={`px-3 py-1 rounded-full border text-brown-900 ${filter === "all" ? "bg-amber-100 text-amber-900" : "bg-amber-900 text-amber-100"}`}
-                                >
-                                    All
-                                </button>
-                                <button
-                                    onClick={() => setFilter("active")}
-                                    className={`px-3 py-1 rounded-full border ${filter === "active" ? "bg-amber-100 text-amber-900" : "bg-amber-900 text-amber-100"}`}
-                                >
-                                    Active
-                                </button>
-                                <button
-                                    onClick={() => setFilter("completed")}
-                                    className={`px-3 py-1 rounded-full border ${filter === "completed" ? "bg-amber-100 text-amber-900" : "bg-amber-900 text-amber-100"}`}
-                                >
-                                    Completed
-                                </button>
-                            </div>
 
+                    </div>
+                </div>
 
-                            {/* <div className="text-sm text-gray-600">{todos.filter((t) =& gt; !t.done).length} items left</div> */}
+                <div className="md:flex-row items-center justify-between mb-12
+                    bg-white backdrop-blur-md p-4 rounded-3xl 
+                    border border-white shadow-sm
+                    w-full md:w-auto"
+                >
+
+                    {/* controls */}
+                    <div className="flex flex-col sm:flex-row sm:justify-between items-start sm:items-center gap-3 mb-4 text-white text-xs">
+                        <div className="flex gap-2">
+
+                            <button
+                                onClick={() => setFilter("active")}
+                                className={`px-3 py-1 rounded-full border ${filter === "active" ? "bg-orange-100 text-orange-600" : "bg-orange-600 text-orange-100"}`}
+                            >
+                                Active
+                            </button>
+                            <button
+                                onClick={() => setFilter("completed")}
+                                className={`px-3 py-1 rounded-full border ${filter === "completed" ? "bg-orange-100 text-orange-600" : "bg-orange-600 text-orange-100"}`}
+                            >
+                                Completed
+                            </button>
+                            <button
+                                onClick={() => setFilter("all")}
+                                className={`px-3 py-1 rounded-full border ${filter === "all" ? "bg-orange-100 text-orange-600" : "bg-orange-600 text-orange-100"}`}
+                            >
+                                All
+                            </button>
                         </div>
 
-                        {/* list */}
-                        <ul className="space-y-2">
-                            {filteredTodos().map((todo) => (
-                                <li key={todo.id}
-                                    className="
+
+                        {/* <div className="text-sm text-gray-600">{todos.filter((t) =& gt; !t.done).length} items left</div> */}
+                    </div>
+
+                    {/* list */}
+                    <ul className="space-y-2">
+                        {filteredTodos().map((todo) => (
+                            <li key={todo.id}
+                                className="
                             flex items-center justify-between 
-                            bg-grey shadow-sm 
-                            border border-amber-300 
-                            rounded-md p-3 
-                            text-amber-800
-                            hover:border-amber-600
-                            hover:text-amber-900">
+                            bg-grey shadow-xs 
+                            font-medium
+                            border-b border-r border-orange-100 
+                            rounded-md p-3
+                            text-orange-800
+                            hover:border-orange-100
+                            hover:text-orange-900
+                            hover:shadow-orange-300">
 
-                                    <div className="flex items-center gap-3">
-                                        <input
-                                            type="checkbox"
-                                            value={todo.done}
-                                            onChange={() => toggleDone(todo.id)}
-                                            className="w-5 h-5"
-                                            checked={todo.done}
-                                        />
-                                        <div className={`text-sm ${todo.done ? "line-through text-gray-400" : ""}`}>
-                                            {todo.text}
-                                        </div>
+                                <div className="flex items-center gap-3">
+                                    <input
+                                        type="checkbox"
+                                        value={todo.done}
+                                        onChange={() => toggleDone(todo.id)}
+                                        className="w-4 h-4 accent-orange-400"
+                                        checked={todo.done}
+                                    />
+                                    <div className={`text-sm ${todo.done ? "line-through text-gray-400" : ""}`}>
+                                        {todo.text}
                                     </div>
+                                </div>
 
-                                    <div className="flex items-center gap-2">
-                                        <button onClick={() => startEdit(todo)} className="px-2 py-1 text-xs rounded-md border text-blue-300 hover:text-blue-200">
-                                            Edit
-                                        </button>
-                                        <button onClick={() => deleteTodo(todo.id)} className="px-2 py-1 text-xs rounded-md border text-red-300 hover:text-red-400">
-                                            Delete
-                                        </button>
-                                    </div>
+                                <div className="flex items-center gap-2">
+                                    <button onClick={() => startEdit(todo)} className="px-2 py-1 text-xs rounded-md border-2 border-orange-300 text-blue-300 hover:text-blue-400">
+                                        <Pencil size={13} />
+                                    </button>
+                                    <button onClick={() => deleteTodo(todo.id)} className="px-2 py-1 text-xs rounded-md border-2 border-orange-300 text-red-300 hover:text-red-400">
+                                        <Trash2 size={13} />
+                                    </button>
+                                </div>
 
-                                </li>
-                            ))}
-                        </ul>
+                            </li>
+                        ))}
+                    </ul>
 
 
-                        {/* footer actions */}
-                        <div className="mt-4 flex items-center justify-between">
-                            <button onClick={clearComplete} className="px-3 py-2 bg-amber-600 rounded-md border text-sm text-white">
+                    {/* footer actions */}
+                    <div className="mt-4 flex items-center justify-between">
+                        <button className="px-3 py-2 bg-red-800 rounded-xl border text-sm text-white"
+                            onClick={() => {
+                                if (!confirm("Yakin hapus semua?")) return;
+                                setTodos([]);
+                            }}>
+                            Clear All
+                        </button>
+
+                        {filter === "completed" &&
+                            <button onClick={clearComplete} className="px-3 py-2 bg-orange-600 rounded-xl border text-sm text-white">
                                 Clear completed
                             </button>
-
-                            <button className="px-3 py-2 bg-red-800 rounded-md border text-sm text-white"
-                                onClick={() => {
-                                    if (!confirm("Yakin hapus semua?")) return;
-                                    setTodos([]);
-                                }}>
-                                Clear All
-                            </button>
-                        </div>
+                        }
                     </div>
                 </div>
             </div>
